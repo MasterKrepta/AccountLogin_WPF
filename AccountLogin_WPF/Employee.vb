61f5aeb759dc58aa6597d79b412fa1730d4bb891
MasterKrepta <brianj.pajak@gmail.com>
2018-11-02 21:40:56 -0400

    sqlite added

---
+++ AccountLogin_WPF/Employee.vb
@@ -0,0 +1,32 @@
+Imports AccountLogin_WPF
+
+Public Class Employee
+
+    Public Enum EmployeeType
+        Production
+        Office
+        Supervisor
+        Manager
+    End Enum
+
+    Public Name As String
+    Public Type As EmployeeType
+    Public Title As String
+    Public PayRate As Double
+
+    Public Sub New(name As String, type As EmployeeType, title As String, pay As Double)
+        Me.Name = name
+        Me.Type = type
+        Me.Title = title
+        Me.PayRate = pay
+        'TODO Add this to the data employees list
+    End Sub
+
+    Public Sub Description()
+        Console.WriteLine("Employee Name: " + Name)
+        Console.WriteLine("-----------------------")
+        Console.WriteLine("Employee Type: " + Type)
+        Console.WriteLine("Job Title: " + Title)
+        Console.WriteLine("PayRate: " + PayRate + " per Hour.")
+    End Sub
+End Class
